439d3bff74198b74068962c7d3686413f88a8c41
qisong.xu <1656655730@qq.com>
2019-06-21 15:59:50 +0800

    first commit

---
+++ headers.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author:xxx time:2019/6/18
+headers = {
+
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.90 Safari/537.36'
+
+}
